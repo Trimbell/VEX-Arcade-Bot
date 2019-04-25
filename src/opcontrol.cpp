a8525a731e545b3675dd9e68863100cd22eda543
Mason Trimbell <mtmaster3@gmail.com>
2019-04-25 16:54:06 -0700

    Upload arcade code

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+auto dt = ChassisControllerFactory::create(
+  {1, 2},
+  {-3, -4},
+  AbstractMotor::gearset::green
+);
+
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -14,18 +20,14 @@
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+	Controller master;
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+	while (true) {
+		dt.arcade(
+      master.getAnalog(ControllerAnalog::leftY),
+      master.getAnalog(ControllerAnalog::rightX)
+    );
+    
+    pros::Task::delay(10);
 	}
 }
